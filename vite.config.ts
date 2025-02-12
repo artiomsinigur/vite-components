614d7f3ae7a4262e07359440e6f8c3aa17da294b
Artiom <sinigur2006@gmail.com>
2025-02-11 22:46:31 -0500

    config build property for Library Mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    build: {
+        lib: {
+            entry: resolve(__dirname, 'src/components'),
+            name: 'MyLib',
+            fileName: 'my-design-system',
+            formats: ['es' ,'umd'],
+        },
+        rollupOptions: {
+            external: ['react', 'react-dom', 'react/jsx-runtime'],
+            output: {
+                globals: {
+                    react: 'React',
+                    'react-dom': 'ReactDOM',
+                },
+            },
+        }
+    },
 });
