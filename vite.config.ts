0b593fd8db6f27bf6c0300b6aabd223a9dc4b44c
Artiom <sinigur2006@gmail.com>
2025-02-13 09:23:09 -0500

    Generate Declarafion Files

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        dts({
+            include: ['src/components/**/*'],
+        }),
+    ],
     build: {
+        copyPublicDir: false,
         lib: {
             entry: resolve(__dirname, 'src/components'),
             name: 'MyLib',
             fileName: 'my-design-system',
-            formats: ['es' ,'umd'],
+            formats: ['es', 'umd'],
         },
         rollupOptions: {
             external: ['react', 'react-dom', 'react/jsx-runtime'],
@@ -20,6 +27,6 @@ export default defineConfig({
                     'react-dom': 'ReactDOM',
                 },
             },
-        }
+        },
     },
 });
