518ce7f1abed722cae72035565596f99bf9347d2
Artiom <sinigur2006@gmail.com>
2025-02-17 10:18:24 -0500

    TypeScript with CSS Modules

---
+++ src/components/button.module.css.d.ts
@@ -0,0 +1,6 @@
+declare const styles: {
+  readonly "button": string;
+  readonly "dangerous": string;
+};
+export = styles;
+
---
+++ src/index.css.d.ts
@@ -0,0 +1,5 @@
+declare const styles: {
+  readonly "application": string;
+};
+export = styles;
+
